b23521dcb8c657f1b3cad6b856ad42b66803088c
thedunerats <thedunerats@yahoo.com>
2020-03-29 17:29:26 -0700

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms'; //manually typed.
 // this is also where you might add an API key if you need one.
+// don't forget to update the imports here when you install a new dependency.
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,6 +11,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DatabasedemoComponent } from './databasedemo/databasedemo.component';
 import { HttpClientModule } from '@angular/common/http';
 import { TrafficsimComponent } from './trafficsim/trafficsim.component';  // you needed this.
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,8 @@ import { TrafficsimComponent } from './trafficsim/trafficsim.component';  // you
     BrowserAnimationsModule,
     HttpClientModule, // and this too.
     FormsModule, //hand typed.
-    ReactiveFormsModule // hand typed.
+    ReactiveFormsModule, // hand typed.
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
