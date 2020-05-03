d46eef84da36ce282ec1f1430ac6a82e97a84ee8
thedunerats <thedunerats@yahoo.com>
2020-05-03 12:41:56 -0700

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component'; //you need to update this every time you have a new component.
 import { DatabasedemoComponent } from './databasedemo/databasedemo.component'; 
 import { TrafficsimComponent} from './trafficsim/trafficsim.component';
+import { DemopageComponent } from './demopage/demopage.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent }, //empty component path loads first
   { path: 'app-databasedemo', component: DatabasedemoComponent }, // you have to update this too. Do it manually if you need to.
-  { path: 'app-trafficsim', component: TrafficsimComponent } 
+  { path: 'app-trafficsim', component: TrafficsimComponent },
+  { path: 'app-demopage', component: DemopageComponent}
 ];
 
 @NgModule({
