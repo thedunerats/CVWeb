c9fc06f0c90373e9f7996406c4c46aabbfcf1615
thedunerats <thedunerats@yahoo.com>
2020-05-24 12:02:46 -0500

    fixed rogue animation bug

--- src/app/sortingvisualizer/sortingvisualizer.component.ts
+++ src/app/sortingvisualizer/sortingvisualizer.component.ts
@@ -21,6 +21,7 @@ export class SortingvisualizerComponent implements OnInit {
 
   //Initialized data:
   pageArray: number[] = [];
+  colorArray: string[] = [];
 
   //functions we need:
   //sorting algorithms
@@ -31,11 +32,13 @@ export class SortingvisualizerComponent implements OnInit {
   fillArray(){
     for(let i = 0; i < 39; i++){ //array of size 40
       this.pageArray.push(Math.round(Math.random() * 40)); //add a random number between 0 and 40
+      this.colorArray.push("black"); //ensures same size as pageArray
     }
     console.log(this.pageArray);
+    console.log(this.colorArray);
   }
 
-  randomizeArray(array: number[]){
+  randomizeArray(){// BE CAREFUL: you may need to reset color array here every time.
     //generates a new array
     this.pageArray = [];
     for(let i = 0; i < 39; i++){ //array of size 40
@@ -55,19 +58,60 @@ export class SortingvisualizerComponent implements OnInit {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
   }
 
+  //need to make everything black by the end of the loop? color first, then delay.
+  // might even consider making the shade function async and calling sleep inside it.
+  shade(id: number, color: string){
+    //let index = id.toString();
+    this.colorArray[id] = color;
+    //console.log(index);//testing
+  }
+
   //SORTING FUNCTIONS
   //bubblesort
+  //FIXME: flipping colors on and off in inner loop isn't perfect. There's a few rogue changes 
+  //in addition to the expected ones. 
+  //It rogue colors the later ones while it goes through the earlier ones.
+  // It happens during the swap. there's an extra delay for some reason.
+  // recognizes a swap, then swaps and the rogue one gets colored.
+  //the rogue one might be the same height as the larger one that
+  // so basically: it happens during a swap if there's a duplicate value?
+  // evene the console cant tell me why this is happening.
+  //the columns at hand also get uncolored during the swap and the duplicate gets colored?
+  // that's REALLY weird. The one that gets swapped toward the right gets uncolored and it's nearest duplicate
+  // gets colored? I need to find another way to do that.
+  // the larger one swaps but it loses its color. I'll see if I can try this a different way.
+  // I could try making the ID more complicated...dunno if that would work.
+  // ^ That did not work. It's not related to the ID. Has to be the duplicated height...
+  // It happens during the swap itself. I took the first shade out and it's still happening.
+  // It might even have to do with the oder of the swap. Maybe I should color the ones that's not
+  // being reassigned first?
+  // A better approach might be to use another array to govern the color, then change that array 
+  // in the typescript? We have to try it.
+  //FIXED: instead of document.getelementbyid, we used an array to hold the ngstyle string property.
  async bubbleSort(array: number[]){
 
     for(let i = 0; i < array.length; i++) {
         for(let j = 0; j < array.length - 1; j++) {
 
+          this.shade(j,"blue");
+          this.shade(j+1, "blue");
+          await this.sleep(10);
+
             if(array[j] > array[j + 1]) {
+
+                this.shade(j+1,"red");
+                this.shade(j, "red");
+                
                 let swap = array[j];
                 array[j] = array[j + 1];
-                await this.sleep(50);
                 array[j + 1] = swap;
+
+                await this.sleep(10);
+
             }
+          
+          this.shade(j,"black");
+          this.shade(j+1, "black");
         }
     }
     console.log(this.pageArray);
@@ -75,7 +119,7 @@ export class SortingvisualizerComponent implements OnInit {
 
   //InsertionSort
   //if a larger element is on the left side of the larger one, it moves the larger element up the index
-  insertionSort(arr:number[]){
+  async insertionSort(arr:number[]){
   if(arr!==undefined){
     for(let i:number = 0; i < arr.length; i++){ // for length of arr
  
@@ -122,7 +166,7 @@ export class SortingvisualizerComponent implements OnInit {
     return i;
   }
   //you need the 3 inputs to do this recursively
-  quickSort(items: number[], left: number, right: number) { //left and right are the upper and lower bounds of the items array
+  async quickSort(items: number[], left: number, right: number) { //left and right are the upper and lower bounds of the items array
     var index;
     if (items.length > 1) {
         index = this.partition(items, left, right); //index returned from partition
