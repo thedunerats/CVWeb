45aa41baa6a1c4cba28f90eee8290fedadc4c9c0
thedunerats <thedunerats@yahoo.com>
2020-05-25 17:46:50 -0500

    quicksort complete

--- src/app/sortingvisualizer/sortingvisualizer.component.ts
+++ src/app/sortingvisualizer/sortingvisualizer.component.ts
@@ -41,8 +41,10 @@ export class SortingvisualizerComponent implements OnInit {
   randomizeArray(){// BE CAREFUL: you may need to reset color array here every time.
     //generates a new array
     this.pageArray = [];
+    this.colorArray = [];
     for(let i = 0; i < 39; i++){ //array of size 40
       this.pageArray.push(Math.round(Math.random() * 40)); //add a random number between 0 and 40
+      this.colorArray.push("black");
     }
     /* randomizes an already existing array
     for(let i = array.length - 1; i > 0; i--){
@@ -93,13 +95,13 @@ export class SortingvisualizerComponent implements OnInit {
     for(let i = 0; i < array.length; i++) {
         for(let j = 0; j < array.length - 1; j++) {
 
-          this.shade(j,"blue");
+          this.shade(j,"blue"); //color to indicate iteration
           this.shade(j+1, "blue");
           await this.sleep(10);
 
             if(array[j] > array[j + 1]) {
 
-                this.shade(j+1,"red");
+                this.shade(j+1,"red"); //swap made
                 this.shade(j, "red");
                 
                 let swap = array[j];
@@ -110,7 +112,7 @@ export class SortingvisualizerComponent implements OnInit {
 
             }
           
-          this.shade(j,"black");
+          this.shade(j,"black"); //continue iterating
           this.shade(j+1, "black");
         }
     }
@@ -119,62 +121,98 @@ export class SortingvisualizerComponent implements OnInit {
 
   //InsertionSort
   //if a larger element is on the left side of the larger one, it moves the larger element up the index
-  async insertionSort(arr:number[]){
+  //REMEMBER: if you want to test your algorithm, run you test sort at the end, not at the beginning. 
+  // It will interfere with the thread working on the sort.
+  // ONLY INCLUDE things you need timed in an aysnc function. Leave everything else outside.
+  // I guess that's why they say embrace javascript's asynchronous nature. 
+async insertionSort(arr:number[]){
+
   if(arr!==undefined){
     for(let i:number = 0; i < arr.length; i++){ // for length of arr
- 
-     let j = i-1;
-     let key = arr[i];
- 
-     while(j>-1 && arr[j]>key){
-       arr[j+1] = arr[j];
-       j--; 
-      }
- 
-     arr[j+1] = key;
- 
-      }
+  
+    let j = i-1;
+    let key = arr[i];
+    
+    this.shade(i,"yellow"); //setting key / pivot value
+    await this.sleep(10);  
+    // this value will be saved until the end when it gets put in the right place.
+
+    while(j>-1 && arr[j]>key){ //doesn't start if j = -1, which is true in the first cycle of the loop
+       this.shade(j,"red"); // coloring value to be moved
+       await this.sleep(10);
+
+       arr[j+1] = arr[j]; //moves until element is smaller than the key value
+       this.shade(j,"black"); // uncoloring current column after move value to be moved
+       await this.sleep(10);
+       j--;
     }
-    console.log(this.pageArray);
+
+    this.shade(i,"black");
+    arr[j+1] = key; //reassignment of key value to the current column
+    await this.sleep(10);
+    }
   }
+}
 
   //QUICKSORT
   //swapping function
-  swap(items: number[], leftIndex: number, rightIndex: number){
-    var temp = items[leftIndex];
+  async swap(items: number[], leftIndex: number, rightIndex: number){
+    this.shade(leftIndex,"red"); //highlighting the swap
+    this.shade(rightIndex,"red");
+    await this.sleep(25);
+    
+    var temp = items[leftIndex]; //perform the actual swap
     items[leftIndex] = items[rightIndex];
     items[rightIndex] = temp;
+
+    this.shade(leftIndex,"black"); //undoing the swap coloration
+    this.shade(rightIndex,"black");
+    await this.sleep(10);
   }
   
-  partition(items: number[], left: number, right: number): number {
+  async partition(items: number[], left: number, right: number) {
     var pivot = items[Math.floor((right + left) / 2)], //middle element
         i = left, //left pointer
         j = right; //right pointer. both of these are arbitrary? check.
     while (i <= j) {
         while (items[i] < pivot) {
+            this.shade(i,"blue");
+            await this.sleep(25); //iterate through
+
+            this.shade(i,"black");
+            await this.sleep(10); //iterate through
             i++;
         }
         while (items[j] > pivot) {
+          this.shade(j,"blue");
+          await this.sleep(25); //iterate through
+
+          this.shade(j,"black");
+          await this.sleep(10); //iterate through
             j--;
         }
         if (i <= j) {
-            this.swap(items, i, j); //swapping two elements
+
+            await this.swap(items, i, j); //swapping two elements
             i++;
             j--;
         }
     }
+    console.log(i);
     return i;
   }
   //you need the 3 inputs to do this recursively
   async quickSort(items: number[], left: number, right: number) { //left and right are the upper and lower bounds of the items array
-    var index;
+    let index;
     if (items.length > 1) {
-        index = this.partition(items, left, right); //index returned from partition
+        index = await this.partition(items, left, right); //index returned from partition (as a promise)
+        console.log(index);
+        //await Promise.all([]) //FIXME: need quicksort calls inside
         if (left < index - 1) { //more elements on the left side of the pivot
-            this.quickSort(items, left, index - 1);
+            await this.quickSort(items, left, index - 1);
         }
         if (index < right) { //more elements on the right side of the pivot
-            this.quickSort(items, index, right);
+            await this.quickSort(items, index, right);
         }
     }
   }
